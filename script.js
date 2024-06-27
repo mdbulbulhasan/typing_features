762d252fd55c5d56627df13f2f7b8b1279d459bf
Md Bulbul Hasan <bulbulhasan095@gmail.com>
2024-06-27 10:19:12 +0600

    Add Typing feature
     Add Typing feature

---
+++ script.js
@@ -0,0 +1,16 @@
+// Typing Feature Start
+$(document).ready(function(){
+    $(window).scroll(function(){
+
+    });
+
+
+    var typed = new Typed (".typing", {
+        // .typing is class which is belong to html file
+        strings: ["Developer", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 80,
+        loop:true
+    });
+})
+// Typing Feature End
